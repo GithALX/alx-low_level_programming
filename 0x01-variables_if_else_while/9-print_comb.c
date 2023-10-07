8f6d5dc38cad7fdc79d721be11e73d41cdd2707b
Souhail Ouichou <ouichoueducational@gmail.com>
2023-10-07 03:13:56 -0700

    this is the 0x01. C - Variables, if, else, while mandatory task commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 
 /**
- * main - Determines if a number is positive, negative or zero.
+ * main - print 0,1, 9
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	int digit = 0, digit2;
+	int digit = 0
 
 	while (digit1 <= 9)
 	{
