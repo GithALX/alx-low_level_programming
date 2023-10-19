1f02881cb3f3956afed551e6a77e97cc3ece1299
Souhail Ouichou <ouichoueducational@gmail.com>
2023-10-19 15:24:46 -0700

    this is the 0x06. C - More pointers, arrays and strings mandatory task commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,22 +8,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i, j;
 
 	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
+	while (dest[i])
+		c++;
+	for (c2 = 0; src[j]; j++)
+		dest[c++] = src[j];
 
-	dest[i] = '\0';
 	return (dest);
 }
