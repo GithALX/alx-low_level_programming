281976960ea19aaf2affac6c52ac2326c585c8de
Souhail Ouichou <ouichoueducational@gmail.com>
2024-03-07 04:59:25 -0800

    this is 0x17. C - Doubly linked lists mandatory task commit

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds a new node at
+ * the end of a dlistint_t list
+ *
+ * @head: head of the list
+ * @n: the element's value
+ * Return: the new element's address
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *h;
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	h = *head;
+
+	if (h != NULL)
+	{
+		while (h->next != NULL)
+			h = h->next;
+		h->next = new;
+	}
+	else
+	{
+		*head = new;
+	}
+
+	new->prev = h;
+
+	return (new);
+}
