474246f63084ea722769747dfa2f70b87c90d146
Souhail Ouichou <ouichueducational@gmail.com>
2024-10-04 09:05:29 -0700

    upgraded the code with minimum lines

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,48 +1,25 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * str_concat - get ends of input and add together for size
  * @s1: input one to concat
  * @s2: input two to concat
- * Return: concat of s1 and s2
+ * Return: (result) on success / NULL on failure
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	char *conct;
-	int i, ci;
+	char *result;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	i = ci = 0;
-	while (s1[i] != '\0')
-		i++;
-	while (s2[ci] != '\0')
-		ci++;
-
-	conct = malloc(sizeof(char) * (i + ci + 1));
-
-	if (conct == NULL)
+	result = malloc(strlen(s1) + strlen(s2) + 1);
+	if (result == NULL)
 		return (NULL);
 
-	i = ci = 0;
-
-	while (s1[i] != '\0')
-	{
-		conct[i] = s1[i];
-		i++;
-	}
-	while (s2[ci] != '\0')
-	{
-		conct[i] = s2[ci];
-		i++, ci++;
-	}
-	conct[i] = '\0';
-
-	return (conct);
-
+	strcpy(result, s1);
+	strcat(result, s2);
+	return (result);
 }
